757d1a843a2d635ee904da52af2a9dece6082e63
JP <jotapemoraez@gmail.com>
2025-10-22 15:06:17 -0300

    Time: 15 ms (14.58%) | Memory: 18.4 MB (14.94%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,33 +1,26 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         
-        if len(flowerbed) == 1:
-            return flowerbed[0] == 0 or n == 0
-        elif len(flowerbed) == 2:
-            return (flowerbed[0] + flowerbed[1] == 0 and n <=1) or (flowerbed[0] + flowerbed[1] == 1 and n ==0)
-        
+        if n == 0: return True
+
         i = 0
         while i < len(flowerbed):
 
-            if i - 1 < 0:
-                if flowerbed[i] == 0 and flowerbed[i+1] == 0:
-                    flowerbed[i] = 1
-                    n -= 1
-                    i += 2
-                else:
-                    i += 1
-            elif i+1 < len(flowerbed) and flowerbed[i-1] == 0 and flowerbed[i+1] == 0 and flowerbed[i] == 0:
+            left_ok = (i == 0) or (flowerbed[i-1] == 0)
+            right_ok = (i + 1 == len(flowerbed)) or flowerbed[i+1] == 0
+            # print(flowerbed, n, i, left_ok, right_ok, (flowerbed[i] == 0) )
+            if left_ok and (flowerbed[i] == 0) and right_ok:
                 flowerbed[i] = 1
-                n -= 1
                 i += 2
-            elif i == len(flowerbed)-1 and flowerbed[i] == 0 and flowerbed[i-1] == 0:
-                flowerbed[i] = 1
                 n -= 1
-                i += 1
+                
             else:
                 i += 1
+            
             if n == 0:
                 return True
+            
+            
             
         
         return n <= 0
