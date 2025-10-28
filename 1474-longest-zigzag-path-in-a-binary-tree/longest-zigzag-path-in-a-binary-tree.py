be73daf4a6a8aea0b4a3aafe51390937553f8189
JP <jotapemoraez@gmail.com>
2025-10-27 22:17:34 -0300

    Time: 47 ms (91.82%) | Memory: 33.1 MB (69.94%) - LeetSync

---
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+
+        maxZig = [0]
+
+        def dfs(node, depth=0, fromSide=0):
+
+            if not node:
+                return
+            
+            if fromSide == 0:
+                if node.right:
+                    maxZig[0] = max(maxZig[0], depth+1)
+                    dfs(node.right, depth+1, 1)
+                if node.left:
+                    maxZig[0] = max(maxZig[0], depth+1)
+                    dfs(node.left, depth+1, 2)
+            elif fromSide == 1:
+                if node.left:
+                    maxZig[0] = max(maxZig[0], depth+1)
+                    dfs(node.left, depth+1, 2)
+                if node.right:
+                    dfs(node.right, 1, 1)
+            else:
+                if node.right:
+                    maxZig[0] = max(maxZig[0], depth+1)
+                    dfs(node.right, depth+1, 1)
+                if node.left:
+                    dfs(node.left, 1, 2)
+        dfs(root)
+
+        return maxZig[0]
+                    
+
+        
